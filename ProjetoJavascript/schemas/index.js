957fdfa2a08a53247100cf6ad4bb3afd351b45ab
Nbreier26 <135669952+Nbreier26@users.noreply.github.com>
2023-09-26 22:00:05 -0300

    a

--- ProjetoJavascript/schemas/index.js
+++ ProjetoJavascript/schemas/index.js
@@ -1,2 +1,11 @@
-export const schemaTypes = []    
+import dados from './Noticia'
+import metodos from './metodos'
+import certificado from './certificado'
+import premio from './premio'
+import listaPremios from './listaPremios'
+import preClub from './preClub'
+
+export const schemaTypes = [metodos, dados, certificado, premio, listaPremios, preClub]
+
+
 
