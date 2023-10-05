ef77fed9e8f8a046d09437b582e6a3637164232a
Nbreier26 <135669952+Nbreier26@users.noreply.github.com>
2023-10-04 21:54:00 -0300

    Update metodos.js

--- ProjetoJavascript/schemas/metodos.js
+++ ProjetoJavascript/schemas/metodos.js
@@ -4,6 +4,11 @@ export default {
     title: 'Método',
     fields: [
       {
+        name: 'ativo',
+        type: 'boolean',
+        title: 'Ativo'
+      },
+      {
         name: 'descricao',
         type: 'text',
         title: 'Descrição'
