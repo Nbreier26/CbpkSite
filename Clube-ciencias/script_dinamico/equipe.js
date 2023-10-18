d15377e80be2ce989b7c4913877b237b32f4f1fb
Nathan Breier <135669952+Nbreier26@users.noreply.github.com>
2023-10-17 21:08:47 -0300

    Equipes

--- Clube-ciencias/script_dinamico/equipe.js
+++ Clube-ciencias/script_dinamico/equipe.js
@@ -1,4 +1,4 @@
-const apiUrl_equipe = 'https://ywf90txg.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%27certificados%27%5D';
+const apiUrl_equipe = 'https://ywf90txg.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%27equipe%27%5D%7B%0A++%22imageUrl%22%3A+Foto.asset-%3Eurl%2C%0A++%0A++...%0A%7D';
 
 
 fetch(apiUrl_equipe)
@@ -16,7 +16,34 @@ fetch(apiUrl_equipe)
 
     result.forEach(equipe => {
       if (equipe.ativo) {
+        
+        const divSep = document.getElementById('sep');
+        const divProfs = document.createElement('div');
+        const divNome = document.createElement('div');
+        const h3 = document.createElement('h3');
+        const p = document.createElement('p');
+        const avatar = document.createElement('img');
+        const section = document.getElementById('equipe');
+
+        divSep.setAttribute("id", "sep");
+        divProfs.classList.add('profs');
+        divNome.setAttribute("id", "nome")
+
+        h3.innerText = equipe.nome;
+        p.innerText = equipe.cargo;
+        avatar.src = equipe.imageUrl;
+
+        divNome.appendChild(h3);
+        divNome.appendChild(p); 
+        divProfs.appendChild(avatar);
+        divProfs.appendChild(divNome);
+        divSep.appendChild(divProfs);
     
+        
+        section.appendChild(divSep);
+
+        console.log(equipe)
+
       }
     });
 
