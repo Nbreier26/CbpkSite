00373fe535e21dce91c78fb46a8c95a8ce32462f
Gustavo Domingos Cortelassi <gutocortelassi72@gmail.com>
2023-10-31 20:59:28 -0300

    att

--- ProjetoJavascript/schemas/horarioPreClube.js
+++ ProjetoJavascript/schemas/horarioPreClube.js
@@ -1,55 +1,55 @@
 export default {
-    name: 'horarioPreClube',
-    type: 'document',
-    title: 'Horários Pré-Clube',
-    fields: [
-      {
-        name: 'turnoManha',
-        type: 'object',
-        title: 'Turno Manhã',
-        fields: [
-            {
-                name: 'inicio',
-                type: 'string',
-                title: 'Inicio'
-            },
-            {
-                name: 'fim',
-                type: 'string',
-                title: 'Fim'
-            }
-        ]
-      },
-      {
-        name: 'turnoTarde',
-        type: 'object',
-        title: 'Turno Tarde',
-        fields: [
-            {
-                name: 'inicio',
-                type: 'string',
-                title: 'Inicio'
-            },
-            {
-                name: 'fim',
-                type: 'string',
-                title: 'Fim'
-            }
-        ]
-      },
-      {
-        name: 'diasDaSemana',
-        type: 'array',
-        title: 'Dias da semana',
-        of: [{type: 'string'}],
-        options: {
-          "Dias": ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira']
-        }
-      },
-      {
-        name: 'turma',
-        type: 'string',
-        title: 'Turma'
+  name: 'horarioPreClube',
+  type: 'document',
+  title: 'Horários Pre-Clube',
+  fields: [
+    {
+      name: 'turnoManha',
+      type: 'object',
+      title: 'Turno Manhã',
+      fields: [
+          {
+              name: 'inicio',
+              type: 'string',
+              title: 'Inicio'
+          },
+          {
+              name: 'fim',
+              type: 'string',
+              title: 'Fim'
+          }
+      ]
+    },
+    {
+      name: 'turnoTarde',
+      type: 'object',
+      title: 'Turno Tarde',
+      fields: [
+          {
+              name: 'inicio',
+              type: 'string',
+              title: 'Inicio'
+          },
+          {
+              name: 'fim',
+              type: 'string',
+              title: 'Fim'
+          }
+      ]
+    },
+    {
+      name: 'diasDaSemana',
+      type: 'boolean',
+      title: 'Dias da semana',
+      of: [{type: 'string'}],
+      options: {
+        "Dias": ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira']
       }
-    ]
-  }
+    },
+    {
+      name: 'turma',
+      type: 'string',
+      title: 'Turma'
+    }
+  ]
+}
