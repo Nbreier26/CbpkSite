a1f3e875e17049bf6b6b3f43ac66acfb97184693
Nbreier26 <135669952+Nbreier26@users.noreply.github.com>
2023-10-31 20:50:39 -0300

    Update horarioClube.js

--- ProjetoJavascript/schemas/horarioClube.js
+++ ProjetoJavascript/schemas/horarioClube.js
@@ -39,7 +39,7 @@ export default {
       },
       {
         name: 'diasDaSemana',
-        type: 'array',
+        type: 'boolean',
         title: 'Dias da semana',
         of: [{type: 'string'}],
         options: {
