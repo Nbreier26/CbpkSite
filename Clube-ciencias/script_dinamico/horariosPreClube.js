f45884faaa89c24c9b89c72e6a5a108d15a0cb54
Nbreier26 <135669952+Nbreier26@users.noreply.github.com>
2023-10-31 20:47:07 -0300

    Horarios js

--- Clube-ciencias/script_dinamico/horariosPreClube.js
+++ Clube-ciencias/script_dinamico/horariosPreClube.js
@@ -1,4 +1,12 @@
+<<<<<<< Updated upstream
 const apiUrl_horarios = 'https://ywf90txg.api.sanity.io/v2021-03-25/data/query/production?query=*%5B_type+%3D%3D+%27horarioPreClube%27%5D%7B%0A++%0A%7D&perspective=previewDrafts';
+=======
+<<<<<<< HEAD:Clube-ciencias/script_dinamico/horarios.js
+const apiUrl_horarios = 'https://ywf90txg.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22horarios%22%5D';
+=======
+const apiUrl_horarios = 'https://ywf90txg.api.sanity.io/v2021-03-25/data/query/production?query=*%5B_type+%3D%3D+%27horarioPreClube%27%5D%7B%0A++%0A%7D&perspective=previewDrafts';
+>>>>>>> 931f65d951850cd72f49ccae82e2210ec25cbfd2:Clube-ciencias/script_dinamico/horariosPreClube.js
+>>>>>>> Stashed changes
 
 fetch(apiUrl_horarios)
     .then(response => {
@@ -10,6 +18,7 @@ fetch(apiUrl_horarios)
     })
     .then(data => {
         const result = data.result;
+<<<<<<< Updated upstream
 
         result.forEach(horario => {
             // Verifique se a turma é a desejada (substitua 'SuaTurma' pelo nome da turma desejada)
@@ -36,6 +45,29 @@ fetch(apiUrl_horarios)
                     quadrado3.appendChild(turnoTardeElement);
                 });
             }
+=======
+        console.log(result)
+
+        result.forEach(horario => {
+<<<<<<< HEAD:Clube-ciencias/script_dinamico/horarios.js
+            
+            const hora1 = document.getElementById('hora_clube');
+            const p = document.createElement('p')
+            const inicio = horario.turnoManha.inicio
+            const fim = horario.turnoManha.fim
+            const modeloHora = inicio + ' - ' + fim;
+=======
+            // Verifique se a turma é a desejada (substitua 'SuaTurma' pelo nome da turma desejada)
+            if (horario.turma === 'SuaTurma') {
+                const quadrado3 = document.getElementById('');
+>>>>>>> 931f65d951850cd72f49ccae82e2210ec25cbfd2:Clube-ciencias/script_dinamico/horariosPreClube.js
+
+            p.innerText = modeloHora
+            hora1.appendChild(p)
+
+            
+            
+>>>>>>> Stashed changes
         });
 
     })
