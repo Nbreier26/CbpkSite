b47add9454b29542ea9160a4f9320da6afecede7
Nbreier26 <135669952+Nbreier26@users.noreply.github.com>
2023-11-14 20:13:34 -0300

    att

--- Clube-ciencias/script_dinamico/duvidas.js
+++ Clube-ciencias/script_dinamico/duvidas.js
@@ -1,4 +1,4 @@
-const apiUrl_duvidas = 'https://ywf90txg.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%27duvidas%27%5D%0A';
+const apiUrl_duvidas = 'https://ywf90txg.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%27duvidas%27+%26%26+ativo+%3D%3D+true+%5D+%7C+order%28_createdAt+asc%29%0A%0A';
 
 fetch(apiUrl_duvidas)
     .then(response => {
@@ -13,8 +13,7 @@ fetch(apiUrl_duvidas)
     .then(data => {
         const result = data.result;
 
-        result.forEach((duvidas, indice) => {
-            if (duvidas.ativo) {
+        result.forEach((duvidas, index) => {
                 const p = document.createElement("p");
                 const frequente = document.getElementById('frequente');
                 const divCollapse = document.createElement("div");
@@ -23,17 +22,17 @@ fetch(apiUrl_duvidas)
                 divCard.className = 'card card-body';
 
                 divCollapse.className = "collapse";
-                divCollapse.id = "collapseExample" + indice;
+                divCollapse.id = "collapseExample" + index;
 
                 const botao = document.createElement("button");
 
                 botao.className = "campo-freq";
                 botao.type = "button";
                 botao.setAttribute("data-bs-toggle", "collapse");
-                botao.setAttribute("data-bs-target", "#collapseExample" + indice);
+                botao.setAttribute("data-bs-target", "#collapseExample" + index);
                 botao.setAttribute("aria-expanded", "false");
-                botao.setAttribute("aria-controls", "collapseExample" + indice);  // Usando o índice no controle
-                botao.textContent = duvidas.pergunta;
+                botao.setAttribute("aria-controls", "collapseExample" + index); 
+                botao.textContent = duvidas.pergunta;index
                 divCard.innerText = duvidas.resposta;
 
                 p.appendChild(botao);
@@ -41,7 +40,7 @@ fetch(apiUrl_duvidas)
                 frequente.appendChild(p);
                 divCollapse.appendChild(divCard);
                 frequente.appendChild(divCollapse);
-            }
+            
         });
 
 
